c64bf3ec29e96d2808d753c265cba8a7a36791a0
NARASIMMA.R <117350135+Narasimma-22it35@users.noreply.github.com>
2026-02-24 15:28:39 +0530

    Time: 22 ms (88.61%), Space: 77.6 MB (90.59%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -12,7 +12,7 @@ class Solution {
 
         for (int i = 1; i < nums.length; i++) {
 
-            if (nums[i] == nums[i - 1]) {
+            if (nums[i] == nums[i-1]) {
                 continue;   // skip duplicates
             }
 
