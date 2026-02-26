82bb4edb0e0a0019179d62ddb67093a7a6e796a5
NARASIMMA.R <117350135+Narasimma-22it35@users.noreply.github.com>
2026-02-26 23:22:04 +0530

    Time: 14 ms (11.18%), Space: 46.8 MB (19.86%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,44 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        int maxcount = 0;
 
-        int maxLength = 0;
-
-        // Generate all substrings
         for (int i = 0; i < s.length(); i++) {
 
-            String temp = "";
+            boolean[] seen = new boolean[256]; 
 
             for (int j = i; j < s.length(); j++) {
 
-                temp += s.charAt(j);
+                char ch = s.charAt(j);
+                if (seen[ch]) {
+                    break;
+                }
 
-                if (isUnique(temp)) {
-                    maxLength = Math.max(maxLength, temp.length());
-                } else {
-                    break; // stop if duplicate found
+                seen[ch] = true;
+
+                int len = j - i + 1;
+                if (len > maxcount) {
+                    maxcount = len;
                 }
             }
         }
-
-        return maxLength;
-    }
-
-    // Function to check uniqueness
-    public boolean isUnique(String str) {
-
-        int[] freq = new int[256];
-
-        for (int i = 0; i < str.length(); i++) {
-
-            char ch = str.charAt(i);
-
-            if (freq[ch] == 1) {
-                return false;
-            }
-
-            freq[ch]++;
-        }
-
-        return true;
+        return maxcount;
     }
 }
